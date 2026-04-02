d0d80f95e0db20337e13cff4ebe9f6c46dca0704
billjohnathan8 <80025972+billjohnathan8@users.noreply.github.com>
2026-04-02 18:57:09 +0800

    fix: removed unused static import assertThatCode

--- services/backend/client/src/test/java/com/scroogebank/crm/client_service/service/SnsEmailPublisherServiceTest.java
+++ services/backend/client/src/test/java/com/scroogebank/crm/client_service/service/SnsEmailPublisherServiceTest.java
@@ -14,7 +14,6 @@ import java.util.Map;
 
 import org.mockito.ArgumentCaptor;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
